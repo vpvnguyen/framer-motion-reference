a04407afb39d40426852cb91579618bf5519e1cb
Vincent Nguyen <vpvnguyen@gmail.com>
2020-09-28 19:47:12 -0700

    import more aniamtion components

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Animation from "./components/Animations/Animation";
 import Animation2 from "./components/Animations/Animation2";
 import Animation3 from "./components/Animations/Animation3";
+import Animation4 from "./components/Animations/Animation4";
+import IndividualLetters from "./components/Animations/IndividualLetters";
+import SingleLetter from "./components/Animations/SingleLetter";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Animation />
       <Animation2 />
       <Animation3 />
+      <Animation4 />
+      <IndividualLetters />
+      <SingleLetter />
     </>
   );
 }
